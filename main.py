843f6efec21207620857caca27fdd33a6635dae5
k1b3r74rd <kibertard@gmail.com>
2020-10-21 16:47:44 +0700

    problem: extra line b4 score

--- main.py
+++ main.py
@@ -1,32 +1,64 @@
-import keyboard, random
+import keyboard, os
 A = []
+protagonist_y = 13
+protagonist_x = 9
+score = 0
 
 
 def greetings():
-    print('To start game, press Enter...')
-    input()
-    sheet()
+    def start(enter):
+        if enter.name == "enter":
+            sheet(protagonist_y, protagonist_x)
+    os.system('cls')
+    print('Welcome to da game. Press "Enter" to start da game.')
+    keyboard.hook(start)
 
 
-def sheet():
-    for i in range(15):
-        if i == 13:
-            A.append(["__"] * 20)
-        elif i == 14:
-            A.append(["  "] * 20)
-            A[i][random.randint(1, 19)] = "^ "
+def sheet(y, x):
+    def protagonist(y, x):
+        A[y][x] = " ^ "
+
+    os.system('cls')
+    for i in range(1, 15):
+        if i == 8:
+            A.append(["_ _"] * 20)
         else:
-            A.append(["* "] * 20)
-        A[i][0]="\  "
+            A.append(["   "] * 20)
+
+        A[i][0]="\ "
         A[i][19] = " /"
 
+    protagonist(y, x)
+    A[0][0] = 'Score: ' + str(score)
     for i in range(15):
         print()
-        # A[i][random.randint(1, 29)] = " * "
         for j in range(20):
             print(A[i][j], end="")
 
 
+def protagonist_move(button):
+    global protagonist_x
+
+    if button.name == "left" and button.event_type == "down":
+        if protagonist_x - 1 == 0:
+            pass
+        else:
+            A[protagonist_y][protagonist_x] = "   "
+            protagonist_x -= 1
+            sheet(protagonist_y, protagonist_x)
+
+    elif button.name == "right" and button.event_type == "down":
+        if protagonist_x + 1 == 19:
+            pass
+        else:
+            A[protagonist_y][protagonist_x] = "   "
+            protagonist_x += 1
+            sheet(protagonist_y, protagonist_x)
+
+
 if __name__ == "__main__":
     greetings()
-    input()
+    while True:
+        keyboard.hook(protagonist_move)
+        keyboard.wait()
+
--- test.py
+++ test.py
@@ -1,24 +1,27 @@
-import keyboard, random
+import keyboard, os
 A = []
+protagonist_y = 13
+protagonist_x = 9
 
 
-def sheet():
-    global protagonist_y
-    global protagonist_x
+def protagonist(y, x):
+    A[y][x] = " ^ "
+
+
+def sheet(y, x):
+    os.system('cls')
     for i in range(15):
-        if i == 8:
-            A.append(["_ _"] * 20)
-        elif i in [9, 10, 11, 12, 14]:
-            A.append(["   "] * 20)
-        elif i == 13:
-            A.append(["   "] * 20)
-            protagonist_y = i
-            protagonist_x = 9
-            A[protagonist_y][protagonist_x] = " ^ "
-        else:
+        if i in range(0, 8):
             A.append([" * "] * 20)
-        A[i][0]="\  "
+        elif i == 8:
+            A.append(["_ _"] * 20)
+        elif i in range(9, 15):
+            A.append(["   "] * 20)
+
+        A[i][0]="\ "
         A[i][19] = " /"
+
+    protagonist(y, x)
 
     for i in range(15):
         print()
@@ -27,14 +30,29 @@ def sheet():
 
 
 def protagonist_move(button):
-    if button.name == "left":
-        pass
+    global protagonist_x
+
+    if button.name == "left" and button.event_type == "down":
+        if protagonist_x - 1 == 0:
+            pass
+        else:
+            A[protagonist_y][protagonist_x] = "   "
+            protagonist_x -= 1
+            sheet(protagonist_y, protagonist_x)
+
+    elif button.name == "right" and button.event_type == "down":
+        if protagonist_x + 1 == 19:
+            pass
+        else:
+            A[protagonist_y][protagonist_x] = "   "
+            protagonist_x += 1
+            sheet(protagonist_y, protagonist_x)
 
 
 def print_pressed_keys(button):
     print(button.event_type,"\n", button.name)
 
 
-sheet()
-# keyboard.hook(print_pressed_keys)
-# keyboard.wait()
+sheet(protagonist_y, protagonist_x)
+keyboard.hook(protagonist_move)
+keyboard.wait()
